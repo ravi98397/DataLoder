2e3a158b5e274ea6d47f35aebcc5cdf359bfbe74
ravi98397 <ravi98397@gmail.com>
2024-09-07 19:18:00 +0530

    inital commit

---
+++ dataloader/src/main/java/com/backend/dataloader/BackendDataloaderApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDataloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDataloaderApplication.class, args);
+	}
+
+}
---
+++ dataloader/src/test/java/com/backend/dataloader/BackendDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.dataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
